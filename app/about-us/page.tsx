5a99f6f59c99b28be69988510aad6706bff5ce93
leejuyeol <ljykimc159@gmail.com>
2024-09-07 18:37:16 +0900

    두번째 커밋

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,5 +2,7 @@ import { useState } from 'react'
 
 export default function aboutUs() {
     const [list, setList] = useState([]);
+    const [list2, setList2] = useState([]);
+    
     return <h1>about us </h1>
 }
