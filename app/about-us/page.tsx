aca4ec408b9db5ebdeeca9fb50ab2495bedf3227
leejuyeol <ljykimc159@gmail.com>
2024-09-08 08:46:53 +0900

    about 주석 추가

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,6 +3,6 @@ import { useState } from 'react'
 export default function aboutUs() {
     const [list, setList] = useState([]);
     const [list2, setList2] = useState([]);
-    
+    // juyeol에서 수정
     return <h1>about us </h1>
 }
