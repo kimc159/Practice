4cc01e2756125097b518de136340fe017247aaf4
leejuyeol <ljykimc159@gmail.com>
2024-09-08 08:48:09 +0900

    doori에서 about 주석 추가

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,6 +3,6 @@ import { useState } from 'react'
 export default function aboutUs() {
     const [list, setList] = useState([]);
     const [list2, setList2] = useState([]);
-    
+    // doori에서 주석 추가
     return <h1>about us </h1>
 }
