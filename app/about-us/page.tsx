ef2eddf508433fc4dc4de4e39fcb54fbbb0e7573
juyeol <ljykimc159@gmail.com>
2025-06-18 15:50:43 +0900

    bb 커밋

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -6,5 +6,12 @@ export default function aboutUs() {
     // 충돌 해결 후 주석
     // 충돌 테스트 두번째 주석
     // 충돌 테스트 세번째 주석
+
+    let bb = 0;
+
+    if(true == true) {
+        bb = 1;
+    }
+    
     return <h1>about us </h1>
 }
